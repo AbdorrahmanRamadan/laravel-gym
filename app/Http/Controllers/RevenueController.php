a58216616c0c1ed389345b1c8365b2a059f2fe9a
passanthamdy <passant.hamdy99@gmail.com>
2022-05-05 21:22:26 +0200

    Revenue Task

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -19,17 +19,23 @@ class RevenueController extends Controller
 
         if($userRole == 'admin'){
             $revenue=BoughtPackage::get()->sum('purchase_price');
+        }else if($userRole == 'city_manager'){
+          $currentUserId=Auth::id();
+           $city_id = CityManager::where('city_manager_id',$currentUserId)->value('city_id');
+           $gymsId = Gym::where('city_id',$city_id)->get()->pluck('id');
+           $revenue_query=DB::table('bought_packages')->select('purchase_price')->whereIn('gym_id',$gymsId)->pluck('purchase_price');
+           $revenue=0;
+            foreach($revenue_query as $gym_revenue){
+                $revenue+= $gym_revenue;
+
+            }
+
+
+         }
+         else if($userRole == 'gym_manager'){
+            $gymId=Auth::id();
+            $revenue=BoughtPackage::where('gym_id',$gymId)->sum('purchase_price');
         }
-        // else if($userRole == 'city_manager'){
-        //     $currentUserId=Auth::id();
-        //     $city_id = CityManager::where('city_manager_id',$currentUserId)->value('city_id');
-        //     $gymsId = Gym::where('city_id',$city_id)->value('id');  //array
-        //     $revenue=DB::table('bought_packages')->select('purchase_price')->whereIn('gym_id',$gymsId)->sum('purchase_price');
-        //     //$revenue=BoughtPackage::where()->sum('purchase_price');
-        // }else if($userRole == 'gym_manager'){
-        //     $gymId=Auth::id();
-        //     $revenue=BoughtPackage::where('gym_id',$gymId)->sum('purchase_price');
-        // }
         return view("Revenue.index",['revenue'=> $revenue]);
     }
 }
