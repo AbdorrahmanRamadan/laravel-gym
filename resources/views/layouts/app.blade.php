2e91be09a0f88b362db828bbeccdaa5546cda291
AbdorrahmanRamadan <AbdorrahmanRamadan@gmail.com>
2022-05-05 22:55:47 +0200

    removed duplicate script from app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -373,9 +373,5 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 @stack('script')
 @endguest
-
-<script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-@stack('script')
 </body>
 </html>
